b37cc9d622074a3246eed0da39cb91735bc76058
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-03-28 19:59:34 +0300

    güncelleme

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Yazılımcı Hanım',
+        'title': 'Blog Post 1',
+        'content': 'Benim ilk blog gönderim',
+        'date_posted': '28 Mart 2024'
+    },
+    {
+        'author': 'Oyuncu',
+        'title': 'Blog Post 2',
+        'content': 'Benim ilk ikinci gönderim',
+        'date_posted': '05 Nisan 2024'
+    },
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Blog Home</h1>")
+
+    context= {
+        'posts':posts
+    }
+    
+    return render(request, 'blog/home.html', context)
+
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html', {'title': "About Pages"})
